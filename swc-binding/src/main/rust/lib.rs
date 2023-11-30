f14fcf41deeb97edd39b4837fea2107dc32873b4
yidafu <me@yidefu.dev>
2023-11-30 23:00:23 +0800

    feat(swc-binding): rust side code copy from swc offical node binding
    see more:
    + https://github.com/swc-project/swc/blob/main/bindings/binding_core_node/src/parse.rs
    + https://github.com/swc-project/swc/blob/main/bindings/binding_core_node/src/transform.rs

--- swc-binding/src/main/rust/lib.rs
+++ swc-binding/src/main/rust/lib.rs
@@ -1,15 +1,23 @@
+use std::any::type_name;
 use std::path::Path;
 use std::sync::Arc;
 
 use anyhow::Context;
+use combine::error::StringStreamError;
+use jni::errors::{Error, Exception};
 use jni::objects::{JClass, JString};
 use jni::sys::{jboolean, jstring};
 use jni::JNIEnv;
 use jni_fn::jni_fn;
 use serde::de::DeserializeOwned;
+use swc::config::ParseOptions;
 use swc::{config::Options, try_with_handler, Compiler};
+use swc_common::comments::Comments;
+use swc_common::Mark;
 use swc_common::{sync::Lazy, FileName, FilePathMapping, SourceMap, GLOBALS};
 use swc_ecma_ast::Program;
+use swc_ecma_transforms_base::resolver;
+use swc_ecma_visit::VisitMutWith;
 
 pub fn deserialize_json<T>(json: &str) -> Result<T, serde_json::Error>
 where
@@ -19,6 +27,47 @@ where
     deserializer.disable_recursion_limit();
 
     T::deserialize(&mut deserializer)
+}
+// format!("{:?}", err)
+pub trait MapErr<T>: Into<Result<T, anyhow::Error>> {
+    fn convert_err(self) -> jni::errors::Result<T> {
+        self.into().map_err(|err| {
+            jni::errors::Error::ParseFailed(
+                StringStreamError::UnexpectedParse,
+                format!("{:?}", err),
+            )
+        })
+    }
+}
+
+impl<T> MapErr<T> for Result<T, anyhow::Error> {}
+
+// pub trait MapErr<T> : Result<T, anyhow::Error> {
+//     fn convert_err(self) -> jni::errors::Result<T> {
+//                 self.into()
+//             .map_err(|err| jni::errors::Error::ParseFailed(StringStreamError::UnexpectedParse, format!("{:?}", err)))
+//     }
+// }
+
+pub fn get_deserialized<T, B>(buffer: B) -> jni::errors::Result<T>
+where
+    T: DeserializeOwned,
+    B: AsRef<[u8]>,
+{
+    let mut deserializer = serde_json::Deserializer::from_slice(buffer.as_ref());
+    deserializer.disable_recursion_limit();
+
+    let v = T::deserialize(&mut deserializer)
+        .with_context(|| {
+            format!(
+                "Failed to deserialize buffer as {}\nJSON: {}",
+                type_name::<T>(),
+                String::from_utf8_lossy(buffer.as_ref())
+            )
+        })
+        .convert_err()?;
+
+    Ok(v)
 }
 
 static COMPILER: Lazy<Arc<Compiler>> = Lazy::new(|| {
@@ -32,28 +81,67 @@ fn get_compiler() -> Arc<Compiler> {
 }
 
 #[jni_fn("dev.yidafu.swc.SwcNative")]
-pub fn sayHello(mut env: JNIEnv, _: JClass, input: JString) -> jstring {
-    let input: String = env
-        .get_string(&input)
-        .expect("Couldn't get java string!")
-        .into();
-
-    let output = env
-        .new_string(format!("Hello, {}!", input))
-        .expect("Couldn't create java string!");
-
-    output.into_raw()
-}
-
-#[jni_fn("dev.yidafu.swc.SwcNative")]
-pub fn parseSync(mut env: JNIEnv, _: JClass, code: JString, options: JString, filename: JString) -> jstring {
-    let input: String = env
+pub fn parseSync(
+    mut env: JNIEnv,
+    _: JClass,
+    code: JString,
+    options: JString,
+    filename: JString,
+) -> jstring {
+    let src: String = env
         .get_string(&code)
         .expect("Couldn't get java string!")
         .into();
+    let c = get_compiler();
+    let options: String = env
+        .get_string(&options)
+        .expect("Couldn't get java string!")
+        .into();
+    let filenameR: String = env
+        .get_string(&filename)
+        .expect("Couldn't get java string!")
+        .into();
+    let refOptions = options.as_bytes();
+    let options: ParseOptions = get_deserialized(refOptions).unwrap();
+    let filename = FileName::Anon;
+    if filenameR.is_empty() {
+        FileName::Anon
+    } else {
+        FileName::Real(filenameR.into())
+    };
 
+    let program = GLOBALS
+        .set(&Default::default(), || {
+            try_with_handler(c.cm.clone(), Default::default(), |handler| {
+                let fm = c.cm.new_source_file(filename, src);
+                let comments = if options.comments {
+                    Some(c.comments() as &dyn Comments)
+                } else {
+                    None
+                };
+                let mut p = c.parse_js(
+                    fm,
+                    handler,
+                    options.target,
+                    options.syntax,
+                    options.is_module,
+                    comments,
+                )?;
+
+                p.visit_mut_with(&mut resolver(
+                    Mark::new(),
+                    Mark::new(),
+                    options.syntax.typescript(),
+                ));
+
+                Ok(p)
+            })
+        })
+        .unwrap();
+
+    let ast_json = serde_json::to_string(&program).unwrap();
     let output = env
-        .new_string(format!("Hello, {}!", input))
+        .new_string(ast_json)
         .expect("Couldn't create java string!");
 
     output.into_raw()
@@ -61,23 +149,54 @@ pub fn parseSync(mut env: JNIEnv, _: JClass, code: JString, options: JString, fi
 
 #[jni_fn("dev.yidafu.swc.SwcNative")]
 pub fn parseFileSync(mut env: JNIEnv, _: JClass, filepath: JString, options: JString) -> jstring {
-    let path: String = env
+    let c = get_compiler();
+    let options: String = env
+        .get_string(&options)
+        .expect("Couldn't get java string!")
+        .into();
+    let filepathR: String = env
         .get_string(&filepath)
         .expect("Couldn't get java string!")
         .into();
-    let c = get_compiler();
-    let fm =    c.cm.load_file(Path::new(path.as_str())).unwrap();
-    let output = GLOBALS
+    let refOptions = options.as_bytes();
+    let options: ParseOptions = get_deserialized(refOptions).unwrap();
+
+    let program = GLOBALS
         .set(&Default::default(), || {
             try_with_handler(c.cm.clone(), Default::default(), |handler| {
-                c.process_js_file(fm, handler, &Default::default())
+                let comments = if options.comments {
+                    Some(c.comments() as &dyn Comments)
+                } else {
+                    None
+                };
+                let fm =
+                    c.cm.load_file(Path::new(filepathR.as_str()))
+                        .expect("failed to read program file");
+                let mut p = c.parse_js(
+                    fm,
+                    handler,
+                    options.target,
+                    options.syntax,
+                    options.is_module,
+                    comments,
+                )?;
+
+                p.visit_mut_with(&mut resolver(
+                    Mark::new(),
+                    Mark::new(),
+                    options.syntax.typescript(),
+                ));
+
+                Ok(p)
             })
         })
         .unwrap();
 
+    let ast_json = serde_json::to_string(&program).unwrap();
     let output = env
-        .new_string(output.code)
-        .expect("Couldn't create Java String");
+        .new_string(ast_json)
+        .expect("Couldn't create java string!");
+
     output.into_raw()
 }
 
@@ -130,23 +249,50 @@ pub fn transformSync(
 pub fn transformFileSync(
     mut env: JNIEnv,
     _: JClass,
-    code: JString,
+    filepath: JString,
     is_module: jboolean,
     options: JString,
 ) -> jstring {
     let c = get_compiler();
-    let src: String = env.get_string(&code).expect("").into();
-    let input = c.cm.new_source_file(FileName::Anon, src);
-    let output = GLOBALS
-        .set(&Default::default(), || {
-            try_with_handler(c.cm.clone(), Default::default(), |handler| {
-                c.process_js_file(input, handler, &Default::default())
-            })
-        })
-        .unwrap();
 
-    let output = env
-        .new_string(output.code)
-        .expect("Couldn't create Java String");
-    output.into_raw()
+    let s: String = env
+        .get_string(&filepath)
+        .expect("Couldn't get java string!")
+        .into();
+    let options_str: String = env
+        .get_string(&options)
+        .expect("Couldn't get java string!")
+        .into();
+
+    let mut options: Options = deserialize_json(&options_str).unwrap();
+
+    if !options.filename.is_empty() {
+        options.config.adjust(Path::new(&options.filename));
+    }
+    // let error_format = options.experimental.error_format.unwrap_or_default();
+    println!("isModule ==> {}", is_module);
+    let output = match is_module {
+        1 => {
+            let program: Program =
+                deserialize_json(s.as_str()).expect("failed to deserialize Program");
+
+            GLOBALS.set(&Default::default(), || {
+                try_with_handler(c.cm.clone(), Default::default(), |handler| {
+                    c.process_js(handler, program, &options)
+                })
+            })
+        }
+        _ => {
+            let fm = c.cm.load_file(Path::new(&s)).expect("failed to load file");
+            GLOBALS.set(&Default::default(), || {
+                try_with_handler(c.cm.clone(), Default::default(), |handler| {
+                    c.process_js_file(fm, handler, &options)
+                })
+            })
+        }
+    };
+
+    env.new_string(output.unwrap().code)
+        .expect("Couldn't create Java String")
+        .into_raw()
 }
