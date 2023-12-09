5ba5f0f3aa9192e3bcd822f4387171da78fe0fb7
yidafu <tiaozuwushi@qq.com>
2023-12-09 11:35:29 +0800

    refactor(swc-binding): modify parseSync signature

--- swc-binding/src/main/kotlin/dev/yidafu/swc/SwcNative.kt
+++ swc-binding/src/main/kotlin/dev/yidafu/swc/SwcNative.kt
@@ -14,59 +14,102 @@ class SwcNative {
         }
     }
 
-    external fun parseSync(code: String, options: String, filename: String?): String
+    external fun parseSync(
+        code: String,
+        options: String,
+        filename: String?,
+    ): String
 
-    external fun parseFileSync(filepath: String, options: String): String
+    external fun parseFileSync(
+        filepath: String,
+        options: String,
+    ): String
 
-    fun parseSync(code: String, options: TsParserConfig, filename: String?): String {
-        return parseSync(code, astJson.encodeToString(options), filename)
+    fun parseSync(
+        code: String,
+        options: ParserConfig,
+        filename: String?,
+    ): String {
+        val optStr = configJson.encodeToString(options)
+        println(optStr)
+        return parseSync(code, optStr, filename)
     }
 
-    fun parseSync(code: String, options: EsParserConfig, filename: String?): String {
-        return parseSync(code, astJson.encodeToString(options), filename)
-    }
-
-    fun parseFileSync(filepath: String, options: TsParserConfig): String {
-        val res = parseFileSync(filepath, astJson.encodeToString(options))
+    fun parseFileSync(
+        filepath: String,
+        options: TsParserConfig,
+    ): String {
+        val res = parseFileSync(filepath, configJson.encodeToString(options))
         return res
     }
-    fun parseFileSync(filepath: String, options: EsParserConfig): String {
-        val res = parseFileSync(filepath, astJson.encodeToString(options))
+
+    fun parseFileSync(
+        filepath: String,
+        options: EsParserConfig,
+    ): String {
+        val res = parseFileSync(filepath, configJson.encodeToString(options))
         return res
     }
 
+    external fun transformSync(
+        code: String,
+        isModule: Boolean,
+        options: String,
+    ): String
 
-    external fun transformSync(code: String, isModule: Boolean, options: String): String
+    external fun transformFileSync(
+        filepath: String,
+        isModule: Boolean,
+        options: String,
+    ): String
 
-    external fun transformFileSync(filepath: String, isModule: Boolean, options: String): String
-
-    fun transformSync(code: String, isModule: Boolean, options: Options): TransformOutput {
-        val optionStr = astJson.encodeToString(options)
+    fun transformSync(
+        code: String,
+        isModule: Boolean,
+        options: Options,
+    ): TransformOutput {
+        val optionStr = configJson.encodeToString(options)
         println(optionStr)
         val res = transformSync(code, isModule, optionStr)
         return astJson.decodeFromString<TransformOutput>(res)
     }
 
-    fun transformFileSync(filepath: String, isModule: Boolean, options: Options): TransformOutput {
-        val res = transformFileSync(filepath, isModule, astJson.encodeToString(options))
+    fun transformFileSync(
+        filepath: String,
+        isModule: Boolean,
+        options: Options,
+    ): TransformOutput {
+        val res = transformFileSync(filepath, isModule, configJson.encodeToString(options))
         return astJson.decodeFromString<TransformOutput>(res)
     }
 
-    external fun printSync(program: String, options: String): String
+    external fun printSync(
+        program: String,
+        options: String,
+    ): String
 
-    fun printSync(program: Program, options: Options): TransformOutput {
+    fun printSync(
+        program: Program,
+        options: Options,
+    ): TransformOutput {
         val pStr = astJson.encodeToString(program)
         println(pStr)
-        val oStr = astJson.encodeToString(options)
+        val oStr = configJson.encodeToString(options)
         val res = printSync(pStr, oStr)
         return astJson.decodeFromString<TransformOutput>(res)
     }
 
-    external fun minifySync(program: String, options: String): String
+    external fun minifySync(
+        program: String,
+        options: String,
+    ): String
 
-    fun minifySync(program: Program, options: Options): TransformOutput {
+    fun minifySync(
+        program: Program,
+        options: Options,
+    ): TransformOutput {
         val pStr = astJson.encodeToString(program)
-        val oStr = astJson.encodeToString(options)
+        val oStr = configJson.encodeToString(options)
         val res = minifySync(pStr, oStr)
         return astJson.decodeFromString<TransformOutput>(res)
     }
