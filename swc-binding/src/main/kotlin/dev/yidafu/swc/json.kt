918191aca33078e1c635df5adc0f0c1e605b2705
yidafu <me@yidefu.dev>
2023-12-17 15:30:11 +0800

    refactor(swc-binding): add utils function paseAstTree

--- swc-binding/src/main/kotlin/dev/yidafu/swc/json.kt
+++ swc-binding/src/main/kotlin/dev/yidafu/swc/json.kt
@@ -1,8 +1,10 @@
 package dev.yidafu.swc
 
+import dev.yidafu.swc.types.Program
 import dev.yidafu.swc.types.configSerializer
 import dev.yidafu.swc.types.swcSerializersModule
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
 @OptIn(ExperimentalSerializationApi::class)
@@ -16,4 +18,8 @@ val astJson = Json {
 val configJson = Json {
     classDiscriminator = "syntax"
     serializersModule = configSerializer
+}
+
+fun parseAstTree(jsonStr: String): Program {
+    return astJson.decodeFromString<Program>(jsonStr)
 }
