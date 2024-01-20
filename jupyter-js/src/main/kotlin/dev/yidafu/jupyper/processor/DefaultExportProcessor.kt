5540c65591f91bc9089affb1ded9eb2089fa3bb3
yidafu <me@yidefu.dev>
2024-01-21 00:05:08 +0800

    docs: update processor comments

--- jupyter-js/src/main/kotlin/dev/yidafu/jupyper/processor/DefaultExportProcessor.kt
+++ jupyter-js/src/main/kotlin/dev/yidafu/jupyper/processor/DefaultExportProcessor.kt
@@ -1,18 +1,37 @@
 package dev.yidafu.jupyper.processor
 
 import dev.yidafu.jupyper.JSX_DEFAULT_EXPORT_VARIABLE_NAME
+import dev.yidafu.swc.dsl.createVariableDeclaration
 import dev.yidafu.swc.dsl.identifier
 import dev.yidafu.swc.dsl.variableDeclarator
 import dev.yidafu.swc.emptySpan
 import dev.yidafu.swc.types.* // ktlint-disable no-wildcard-imports
 
+/**
+ * process JSX default export to variable declaration
+ *
+ * for example:
+ *
+ * ```jsx
+ *  export function App() {
+ *      return <div />
+ *  }
+ * ```
+ * will transform to
+ *
+ * ```js
+ * const __JupyterCellDefaultExportVariable__ = function App() {
+ *      return <div />
+ * }
+ * ```
+ */
 class DefaultExportProcessor : JavaScriptProcessor {
     override fun process(program: Program, context: JavascriptProcessContext) {
         if (program is Module) {
             program.body?.forEachIndexed { index, moduleItem ->
                 if (moduleItem is ExportDefaultDeclaration) {
                     val decl = moduleItem.decl as Node as Expression
-                    val varDecl = VariableDeclarationImpl().apply {
+                    val varDecl = createVariableDeclaration {
                         span = emptySpan()
                         kind = "const"
 
