458ec1ebc7ec16b7ad97a7dab9dff7e92d44e905
yidafu <me@yidefu.dev>
2024-01-27 00:07:22 +0800

    test: update examples/local.js

--- examples/local.js
+++ examples/local.js
@@ -1,18 +1,20 @@
 console.log('local message')
 
-export const var1 = 123, var2 = "123";
+export const foo = 123;
 
-export function FunctionName(){...}
-export class ClassName {...}
+export function functionName() { }
+export class ClassName { }
 
 const var3 = true;
 const var4 = {}
 const var5 = "default"
 export { var3, var4 as obj, var5 as default }
 
+const obj = {a: 1, b: 2};
+export const { a, b: c } = obj;
+
 export default "123"
 
-export default function () {}
+export default function () { }
 
-export default class Xxx {}
-
+export default class Xxx { }
