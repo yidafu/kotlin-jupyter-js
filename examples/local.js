25c84f635254bc647c6f7f80adb6de93f2c5d092
yidafu <yidafu@hzwaterit.com>
2024-01-26 13:53:39 +0800

    docs: update examples/local.js

--- examples/local.js
+++ examples/local.js
@@ -1,1 +1,18 @@
 console.log('local message')
+
+export const var1 = 123, var2 = "123";
+
+export function FunctionName(){...}
+export class ClassName {...}
+
+const var3 = true;
+const var4 = {}
+const var5 = "default"
+export { var3, var4 as obj, var5 as default }
+
+export default "123"
+
+export default function () {}
+
+export default class Xxx {}
+
