05ebff0a534b8f8c3b64f9c78a1f47b94acf03e2
yidafu <me@yidefu.dev>
2024-08-26 00:04:55 +0800

    chore: add deps

--- jupyter-js/build.gradle.kts
+++ jupyter-js/build.gradle.kts
@@ -36,13 +36,13 @@ dependencies {
 //    implementation(project(":swc-binding"))
     implementation(libs.swc.binding)
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
+    testImplementation("io.mockk:mockk:1.13.9")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0")
     testImplementation("io.kotest:kotest-assertions-core:5.8.0")
     testImplementation("io.kotest:kotest-property:5.8.0")
     testImplementation("io.kotest:kotest-assertions-json:5.8.0")
-
-    // https://mvnrepository.com/artifact/io.mockk/mockk
-    testImplementation("io.mockk:mockk:1.13.9")
 }
 
 repositories {
